479fcf4a18154f6760e2644651205241d004b6b7
Heretic324 <51122245+Heretic324@users.noreply.github.com>
2020-06-08 21:53:37 +0800

    Add files via upload

---
+++ 天知道/JS/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        city:'',
+        weatherList:[]
+    },
+    methods:{
+        searchWeather:function(){
+            var that = this;
+            axios.get('http://wthrcdn.etouch.cn/weather_mini?city='+this.city)
+            .then(function(response){
+                console.log(response.date.data.forecast);
+                that.weatherList = response.data.data.forecast
+            })
+            .catch(function(err){})
+        }
+    }
+})
